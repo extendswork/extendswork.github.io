0fd75aa1cbd2618590159183eac7bd9e9c0afd1e
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-05-14 02:41:48 +0300

    fix: google translate inactive

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,7 +12,7 @@
 	/>
 </svelte:head>
 <div class="w-full h-full flex flex-col justify-center items-center select-none">
-	<h1 class="text-4xl font-bold">
+	<h1 class="text-4xl font-bold" translate="no">
 		<span class="text-indigo-500">E</span>
 		<span class="text-indigo-500">X</span>
 		<span class="text-indigo-500">T</span>
@@ -21,7 +21,7 @@
 		<span class="text-indigo-500">D</span>
 		<span class="text-indigo-500">S</span>
 	</h1>
-	<h2 class="text-2xl font-bold">
+	<h2 class="text-2xl font-bold" translate="no">
 		<span class="text-indigo-500">W</span>
 		<span class="text-indigo-500">O</span>
 		<span class="text-indigo-500">R</span>
